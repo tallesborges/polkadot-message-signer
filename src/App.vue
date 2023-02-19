8cd1702ff72f7b67a6b8e682a3b37b2964c225ca
Talles Borges <talles.borges92@gmail.com>
2023-02-19 11:49:57 -0300

    use keyring to sign the message

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,32 @@
     <h1>Polkadot-JS Signer</h1>
     <!-- A select that display all accounts that can be selected  -->
     <div>
-      <p>Select the account to sign a message </p>
-      <select v-model="selectedAccount">
-        <option v-for="account in accounts" :key="account.address" :value="account">
-          {{ account.meta.name }} ({{ account.address }})
-        </option>
-      </select>
 
-      <p>Public Key: {{ publicKey }}</p>
-      <p>Efinity Address: {{ efinityAddress }}</p>
+      <p>Add an account</p>
+      <textarea v-model="accountMnemonic"/>
+      <br/>
+      <button @click="addAccount">Add account</button>
+      <hr/>
 
-      <!-- A input with the message that will be signed-->
-      <p>Message to sign</p>
-      <textarea class="message" v-model="message"/>
+      <div>
+        <p>Select the account to sign a message </p>
+        <select v-model="selectedAccount">
+          <option v-for="account in accounts" :key="account.address" :value="account">
+            {{ account.meta.name }} ({{ account.address }})
+          </option>
+        </select>
+
+        <p>Public Key: {{ publicKey }}</p>
+        <p>Efinity Address: {{ efinityAddress }}</p>
+
+        <!-- A input with the message that will be signed-->
+        <p>Message to sign</p>
+        <textarea class="message" v-model="message"/>
+        <p>Hex Message</p>
+        <textarea class="message" :value="computedMessage" disabled/>
+        <br/>
+        <hr/>
+      </div>
 
       <!-- A button that sign a message with the selected account -->
       <p>Sign a message with the selected account</p>
@@ -23,24 +36,30 @@
 
       <!-- A input that display the signed message -->
       <p>Signed message</p>
-      <textarea class="signedMessage" :value="signedMessage"/>
+      <textarea class="signedMessage" :value="signedMessage" disabled/>
     </div>
   </div>
 
 </template>
 
 <script setup lang="ts">
-import {web3Accounts, web3Enable, web3FromSource} from '@polkadot/extension-dapp';
-import {encodeAddress, decodeAddress, mnemonicGenerate} from '@polkadot/util-crypto'
-import {stringToHex, u8aToHex} from "@polkadot/util";
+import {encodeAddress, decodeAddress} from '@polkadot/util-crypto'
+import {u8aToHex} from "@polkadot/util";
 import {ref, onMounted, Ref, computed} from "vue";
-import {InjectedAccountWithMeta} from "@polkadot/extension-inject/types";
+import {Keyring} from '@polkadot/ui-keyring';
+import {cryptoWaitReady} from '@polkadot/util-crypto';
+import {KeyringAddress} from "@polkadot/ui-keyring/types";
+import {stringToHex} from '@polkadot/util';
 
-const accounts: Ref<InjectedAccountWithMeta[]> = ref([]);
-const selectedAccount: Ref<InjectedAccountWithMeta | undefined> = ref();
+const accounts: Ref<KeyringAddress[]> = ref([]);
+const selectedAccount: Ref<KeyringAddress | undefined> = ref();
+
 const message: Ref<string> = ref('');
-
 const signedMessage: Ref<string> = ref('');
+const accountMnemonic: Ref<string> = ref('');
+
+const keyring = new Keyring();
+keyring.loadAll({ss58Format: 195, type: 'sr25519'});
 
 const publicKey = computed(() => {
   if (!selectedAccount.value) {
@@ -58,48 +77,48 @@ const efinityAddress = computed(() => {
   return encodeAddress(publicKey.value, 195);
 });
 
-const connect = async () => {
-  const extensions = await web3Enable('Polkadot-JS Signer');
-  if (extensions.length === 0) {
-    throw new Error('no extension');
-  }
+const computedMessage = computed(() => {
+  return stringToHex(message.value);
+});
 
-  accounts.value = await web3Accounts();
-  selectedAccount.value = accounts.value[0];
+const addAccount = async () => {
+  keyring.addUri(accountMnemonic.value);
+  accountMnemonic.value = '';
+}
+
+const connect = async () => {
+  await cryptoWaitReady();
+
+  keyring.accounts.subject.subscribe(() => {
+    accounts.value = keyring.getAccounts();
+
+    if (!selectedAccount.value) {
+      selectedAccount.value = accounts.value[0];
+    }
+  });
+
 };
 
 onMounted(connect);
 
 async function signMessage() {
-  const source = selectedAccount.value?.meta.source;
-  if (!source) {
-    throw new Error('No source');
-  }
-  if (!selectedAccount.value) {
-    throw new Error('No account selected');
+  const address = selectedAccount.value?.address;
+  if (!address) {
+    throw new Error('No address');
   }
 
-  const injector = await web3FromSource(source);
-  const signRaw = injector?.signer?.signRaw;
+  const pair = keyring.getPair(address);
+  pair.unlock();
+  const signResult = pair.sign(stringToHex(message.value))
 
-  if (!signRaw) {
-    throw new Error('No signRaw function');
-  }
-
-  const {signature} = await signRaw({
-    address: selectedAccount.value.address,
-    data: stringToHex(message.value),
-    type: 'bytes'
-  });
-
-  signedMessage.value = signature;
+  signedMessage.value = u8aToHex(signResult);
 }
 
 </script>
 
 <style scoped>
 
-.message, .signedMessage {
+textarea {
   width: 100%;
   height: 60px;
 }
